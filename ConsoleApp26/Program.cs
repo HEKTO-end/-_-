48be6607c919bd9d506dc0f89ec5e373e7b7d6f0
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2019-10-21 22:34:07 +0400

    всё

--- ConsoleApp26/Program.cs
+++ ConsoleApp26/Program.cs
@@ -18,11 +18,11 @@ namespace ConsoleApp26
             {
                 Console.WriteLine($"Периметр треугольника равен = {pipio.Check(a, d, c)}");
                 Console.WriteLine($"Площадь треугольника = {Math.Round(pipio.Area(a,d,c),4)}");                             
-                Console.WriteLine($"Площадь внутренней акружности = {Math.Round(pipio.Area_of_inner_circle(a,d,c),4)}");
+                Console.WriteLine($"Площадь внутренней окружности = {Math.Round(pipio.Area_of_inner_circle(a,d,c),4)}");
             }
             else
             {
-                Console.WriteLine("были введены неккоретные данные");
+                Console.WriteLine("были введены некорретные данные");
             }
             Console.ReadKey();
         }      
