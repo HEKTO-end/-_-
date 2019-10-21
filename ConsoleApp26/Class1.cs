f99e6448716d0aa49a8674e2b56b15a922f49c9a
HEKTO-end <54909485+HEKTO-end@users.noreply.github.com>
2019-10-21 22:25:18 +0400

    я сделал через классы
    мне 2 пятёрки

---
+++ ConsoleApp26/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp26
+{
+    public class Class1 
+    {
+        public int Perimeter(int a, int d, int c) => a + d + c;
+
+        public double Area(int a, int d, int c)
+        {
+            var P = (a + d + c) / 2;
+            double S = Math.Sqrt(P * (P - a) * (P - d) * (P - c));
+            return S;
+        }
+
+        public double Area_of_inner_circle(int a, int d, int c)
+        {
+            var P = (a + d + c) / 2;
+            double S = Math.Sqrt(P * (P - a) * (P - d) * (P - c)); ;
+            double R = S / P;
+            double area_of_inner_circle = 3.14 * Math.Pow(R, 2);
+            return area_of_inner_circle;
+        }
+
+        public int Check(int a, int d, int c)
+        {
+            if (a > 0 & d > 0 & c > 0)
+            {
+                if ((d + c) > a & (a + c) > d & (a + d) > c)
+                {
+                    return Perimeter(a, d, c);
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
--- ConsoleApp26/Program.cs
+++ ConsoleApp26/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-мне 5 я первый !
-    
 
 namespace ConsoleApp26
 {
-    class Program
+    class Program 
     {
         static void Main(string[] args)
         {
@@ -15,55 +13,18 @@ namespace ConsoleApp26
             var d = Convert.ToInt32(Console.ReadLine());
             Console.Write("Введите сторону c = ");
             var c = Convert.ToInt32(Console.ReadLine());
-            if (check(a, d, c) != 0)
+            Class1 pipio = new Class1();
+            if (pipio.Check(a, d, c) != 0)
             {
-                Console.WriteLine($"Периметр треугольника равен = {check(a, d, c)}");
-                Console.WriteLine($"Площадь треугольника = {Math.Round(Area(a,d,c),4)}");                             
-                Console.WriteLine($"Площадь внутренней акружности = {Math.Round(Area_of_inner_circle(a,d,c),4)}");
+                Console.WriteLine($"Периметр треугольника равен = {pipio.Check(a, d, c)}");
+                Console.WriteLine($"Площадь треугольника = {Math.Round(pipio.Area(a,d,c),4)}");                             
+                Console.WriteLine($"Площадь внутренней акружности = {Math.Round(pipio.Area_of_inner_circle(a,d,c),4)}");
             }
             else
             {
                 Console.WriteLine("были введены неккоретные данные");
             }
             Console.ReadKey();
-        }
-
-        static int Perimeter(int a, int d, int c) => a + d + c;       
-
-        static double Area(int a, int d, int c)
-        {
-            var P = (a + d + c) / 2;
-            double S = Math.Sqrt(P * (P - a) * (P - d) * (P - c));
-            return S;
-        }
-
-        static double Area_of_inner_circle(int a, int d, int c)
-        {
-            var P = (a + d + c) / 2;
-            double S = Math.Sqrt(P * (P - a) * (P - d) * (P - c)); ;
-            double R = S / P;   
-            double area_of_inner_circle = 3.14 * Math.Pow(R, 2);
-            return area_of_inner_circle;
-        }
-
-        static int check(int a, int d, int c)
-        {
-            if (a > 0 & d > 0 & c > 0)
-            {
-                if ((d + c) > a & (a + c) > d & (a + d) > c)
-                {
-                    return Perimeter(a, d, c);                    
-                }
-                else
-                {
-                    return 0;
-                }
-            }
-            else
-            {
-                return 0;
-            }
-
-        }
+        }      
     }
 }
